635c5314e52130380d6b4edf2989dfd9d2ba02b5
PerdimiotracontraRoblox <matheotechera@icloud.com>
2026-02-01 16:00:27 -0300

    Added the new stages.
    You don't need the old ones, don't you?

---
+++ Killstreak's Charts/stages/TsukaretaOpposition.lua
@@ -0,0 +1,13 @@
+
+
+function onCreatePost()
+	makeLuaSprite('front','ThanosGround',550,550)
+	addLuaSprite('front',false)
+	setScrollFactor('front', 1,1)
+	
+	makeLuaSprite('back', 'oppo/OppositonLol', -1400, -1400);
+	setScrollFactor('back', 0, 0);
+	scaleObject('back', 3, 3)
+	addLuaSprite('back', false);
+	addGlitchEffect('back', 2,5,0.1)
+end
